9590f4522ba3f5f20f828150d42442908976587b
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2022-08-11 11:29:45 +0200

    Remove unmount method for plugin

--- frontend/src/plugin.js
+++ frontend/src/plugin.js
@@ -20,20 +20,14 @@ import ReactDOM from "react-dom";
 
 import ProcessInstanceCount from "./ProcessInstanceCount";
 
-let container = null;
-
 export default {
   id: "process-instance-count",
   pluginPoint: "cockpit.dashboard",
   render: (node, { api }) => {
-    container = node;
     ReactDOM.render(
       <ProcessInstanceCount camundaAPI={ api } />,
-      container
+        node
     );
-  },
-  unmount: () => {
-    ReactDOM.unmountComponentAtNode(container);
   },
 
   // make sure we have a higher priority than the default plugin
