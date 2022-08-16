5b6f33a3ab28026fc156da02e2b8f9785dc775fe
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2022-08-16 14:32:42 +0200

    Place process token for activites not related to hybrid programs

--- frontend/src/process-instance-diagram-overlay/ViewOverlay.js
+++ frontend/src/process-instance-diagram-overlay/ViewOverlay.js
@@ -10,6 +10,7 @@
  */
 
 import './process-instance-diagram.scss'
+import '../camunda.scss'
 
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import camundaModdlePackage from 'camunda-bpmn-moddle/resources/camunda';
@@ -79,7 +80,7 @@ export async function renderOverlay(viewer, camundaAPI, processInstanceId) {
     console.log('Currently active activities to visualize: ', activeActivity);
 
     // visualize process token for retrieved active activities
-    activeActivity.forEach(activeActivity => visualizeActiveActivities(activeActivity['activityId'], overlays, quantmeModeler, viewerElementRegistry));
+    activeActivity.forEach(activeActivity => visualizeActiveActivities(activeActivity['activityId'], overlays, quantmeElementRegistry, viewerElementRegistry, rootElement));
 }
 
 /**
@@ -130,17 +131,38 @@ async function getActiveActivities(camundaAPI, processInstanceId) {
  *
  * @param activeActivityId the ID of the activity to visualize the process token for
  * @param overlays the set of overlays to add the visualization
- * @param quantmeModeler the modeler with the process view to retrieve the coordinates for the token
+ * @param quantmeElementRegistry the element registry of the process view to retrieve the coordinates for the token
  * @param viewerElementRegistry the element registry of the viewer to retrieve all required details about the active activity
+ * @param rootElement the root element to use as parent for adding overlays
  */
-function visualizeActiveActivities(activeActivityId, overlays, quantmeModeler, viewerElementRegistry) {
+function visualizeActiveActivities(activeActivityId, overlays, quantmeElementRegistry, viewerElementRegistry, rootElement) {
     console.log('Visualizing process token for active activity with ID: ', activeActivityId);
 
     // get activity from executed workflow related to given ID
-    let activeActivity = viewerElementRegistry.get(activeActivityId);
+    let activeActivity = viewerElementRegistry.get(activeActivityId).businessObject;
     console.log('Retrieved corresponding activity object: ', activeActivity);
 
-    // TODO
+    // retrieve attributes comprising relevant information about hybrid programs
+    let activeActivityAttributes = activeActivity.$attrs;
+    console.log('Found attributes: ', activeActivityAttributes);
+
+    // handle activities related to hybrid program executions and regular activities differently
+    if (activeActivityAttributes['quantme:hybridRuntimeExecution'] !== undefined && activeActivityAttributes['quantme:hybridRuntimeExecution'] === true) {
+        console.log('Active activity belongs to hybrid program execution with ID: ', activeActivityAttributes['quantme:hybridProgramId']);
+
+        // TODO
+    } else{
+        // if activity is not part of a hybrid program execution it was not changed during rewrite and an activity with the same ID is part of the process view
+        console.log('Active activity is regular activity of the executed workflow. Adding process token...');
+        let activityInView = quantmeElementRegistry.get(activeActivityId);
+        console.log('Found activity with same ID in process view: ', activityInView);
+
+        // add overlay to the retrieved root element
+        overlays.add(rootElement, 'process-view-overlay', {
+            position: {left: activityInView.x - 10, top: activityInView.y + activityInView.height - 10},
+            html: '<span class="badge instance-count" data-original-title="" title="">1</span>'
+        });
+    }
 }
 
 /**
@@ -215,31 +237,25 @@ function updateViewBox(svg, elementRegistry) {
         }
     }
 
-    console.log('Minimum x value for candidate: ', result.minX);
-    console.log('Minimum y value for candidate: ', result.minY);
     console.log('Maximum x value for candidate: ', result.maxX);
     console.log('Maximum y value for candidate: ', result.maxY);
 
-    let width, height, x, y;
-    if (result.minX === undefined || result.minY === undefined || result.maxX === undefined || result.maxY === undefined) {
+    let width, height;
+    if (result.maxX === undefined || result.maxY === undefined) {
         console.log('Error: unable to find modeling element with minimum and maximum x and y values!');
 
         // default values in case an error occurred
         width = 1000;
         height = 1000;
-        x = 0;
-        y = 0;
     } else {
 
         // calculate view box and add a margin of 10 to the min/max values
-        x = result.minX - 10;
-        y = result.minY - 10;
-        width = result.maxX - result.minX + 20;
-        height = result.maxY - result.minY + 20;
+        width = result.maxX + 10;
+        height = result.maxY + 10;
     }
 
     return svg.replace('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0" viewBox="0 0 0 0" version="1.1">',
-        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="' + height + '" viewBox="' + x + ' ' + y + ' ' + width + ' ' + height + '" version="1.1">');
+        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="' + height + '" viewBox="0 0 ' + width + ' ' + height + '" version="1.1">');
 }
 
 /**
