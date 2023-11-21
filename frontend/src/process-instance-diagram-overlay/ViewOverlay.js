8a4f66b18713f73c7aaa96453769614c97e1b21c
LaviniaStiliadou <livia_16@live.de>
2023-11-21 13:19:23 +0100

    use actual quantum view

--- frontend/src/process-instance-diagram-overlay/ViewOverlay.js
+++ frontend/src/process-instance-diagram-overlay/ViewOverlay.js
@@ -61,8 +61,10 @@ export async function renderOverlay(viewer, camundaAPI, processInstanceId) {
 
     console.log("View to generate overlay for is represented by the following XML: ", activeViewXml);
     console.log(viewer.get("canvas"))
-    viewer.importXML('<?xml version="1.0" encoding="UTF-8"?><bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd">  <bpmn2:process id="Process_1" isExecutable="true">    <bpmn2:startEvent id="StartEvent_1" />  </bpmn2:process>  <bpmndi:BPMNDiagram id="BPMNDiagram_1">    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">      <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">        <dc:Bounds x="412" y="240" width="36" height="36" />      </bpmndi:BPMNShape>    </bpmndi:BPMNPlane>  </bpmndi:BPMNDiagram></bpmn2:definitions>');
-
+    viewer.importXML(activeViewXml)
+    elementArray = viewerElementRegistry.getAll();
+    console.log('Diagram contains the following elements: ', elementArray);
+    
     // create modeler capable of understanding the QuantME modeling constructs
     //let quantmeModeler = await createQuantmeModelerFromXml(activeViewXml);
     //console.log('Successfully created QuantME modeler to visualize QuantME constructs in process views: ', quantmeModeler);
